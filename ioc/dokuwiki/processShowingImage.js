d26fc8b278bcdfda0312f85d8eca347f88ba6016
Dani Criado <dani.criado.casas@gmail.com>
2014-11-25 00:50:45 +0100

    fix some issues from processShowingImage.js

--- ioc/dokuwiki/processShowingImage.js
+++ ioc/dokuwiki/processShowingImage.js
@@ -3,40 +3,48 @@ define([
     ,"ioc/gui/IocButton"
     , "dijit/Dialog"
     , "dijit/form/Button"
-//], function(iocButton, Dialog, Button) { //Cal importar el constructir del DOM
-], function(domConstruct, iocButton, Dialog, Button) { 
-//    var res = function(content, imageTitle, imageName) { //NOmés es passa un paràmetre! un objecte que conté les dades en el seu atribut
+    , "dojo/on"
+    , "dojo/_base/event"
+], function(domConstruct, iocButton, Dialog, Button, on, event) { 
     var res = function(params) { 
-        var content = params.content, imageTitle= params.imageTitle, imageName=params.imageName; //adaptació al teu sistema
         var imageDialog = new Dialog({
-             title: imageTitle
-            //,content: content, //Modifiquem el content per afegir-hi la barra de control 
-            ,id: "imageDialog" + "_" + imageName
+             title: params.imageTitle
+             ,onClose: function(e){event.stop(e);fOnClose();} //NO FUNCIONA, e.preventDefault() tampoc em funciona
+            ,id: "imageDialog" + "_" + params.imageId
             //,style: "width: 500px"
         });
 
 //        var actionBar = dojo.create("div", {   //No facis servir variables globals com dijit o dojo. Importa la funcionalitat equivalent!
         var dialogContainer = domConstruct.create("div", {
-           id:"dialogContainer" + "_" + imageName
+           id:"dialogContainer" + "_" + params.imageId
         });
         var contentContainer = domConstruct.create("div", {
            class:"dijitDialogPaneContentArea"            
-        }).innerHTML=content;
+        }).innerHTML=params.content;
         var actionBar = domConstruct.create("div", {
             "class": "dijitDialogPaneActionBar"
         });
-
+        
+        var fOnClose = function(){
+            console.log("DESTROY");
+            //imageDialog.destroyRecursive();
+        };
+        
+        on(imageDialog, "close", function(e){fOnClose();});
+        
         new iocButton({
-            "label": "TODO"
+            "label": params.modifyImageLabel
+            ,"onClick": function(e){
+                //TODO call ajaxcommand?
+                fOnClose();
+            }
         }).placeAt(actionBar);
         
         new Button({
-            "label": "Cancel",
-            "onClick": function(evt) {
-                //dijit.byId("imageDialog").destroyRecursive(); //1) La variable imageDialog encara existeix! Per què no fer-la servir?  2) No facis servir variables globals com dijit o dojo. Importa la funcionalitat equivalent!
-                imageDialog.destroyRecursive();
-            }
+            "label": params.closeDialogLabel
+            ,"onClick": function(e){fOnClose();}
         }).placeAt(actionBar);
+        
         
         //col·locar en el lloc adequat
         domConstruct.place(contentContainer, dialogContainer, "last");
