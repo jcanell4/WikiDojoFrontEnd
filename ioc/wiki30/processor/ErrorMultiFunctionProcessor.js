ae484624aa285e8534e514f9b5b832aa760d636f
jcanell4 <jcanell4@ioc.cat>
2014-12-02 17:26:46 +0100

    Renaming component

--- ioc/wiki30/processor/ErrorMultiFunctionProcessor.js
+++ ioc/wiki30/processor/ErrorMultiFunctionProcessor.js
@@ -3,7 +3,7 @@ define([
     "ioc/wiki30/processor/ErrorProcessor"
 ], function (declare, ErrorProcessor) {
 
-    var ret = declare("ioc.wiki30.processor.ErrorWithResendingProcessor", 
+    var ret = declare("ioc.wiki30.processor.ErrorMultiFunctionProcessor", 
         [ErrorProcessor],
         /**
          * @class ErrorProcessor
