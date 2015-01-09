c86fa2ce0f88148ba241a3e90497c49c0f8537ff
jcanell4 <jcanell4@ioc.cat>
2015-01-09 23:46:05 +0100

    UpdateViewByModesHandler class to process diferent functions linked by modes according to current mode

---
+++ ioc/wiki30/UpdateViewByModesHandler.js
@@ -0,0 +1,47 @@
+define([
+    "dojo/_base/declare" // declare
+   ,"ioc/wiki30/dispatcherSingleton"
+], function (declare, dispatcher) {
+    var ret = declare("ioc.wiki30.UpdateViewByModesHandler", [],
+        /**
+         * @class UpdateViewHandler
+         */
+        {
+            functions:null,
+            /**
+             * Aquest objecta proporciona el métode UpdateViewHandler al dispatcher. Es cridat desde scriptsRef.tpl.
+             * El métode update permet mostrar o ocultar els botons de la part dreta de la pantalla, segons el GlobalState
+             * i la acció a realitzar a la pàgina sel·leccionada.
+             *
+             * @param {function()} updateFunction hashMap de funcions 
+             * associades a modes
+             * @constructor
+             */
+            constructor: function (updateFunctionHashMap) {
+                this.functions = {};
+                if (updateFunctionHashMap) {
+                    for(var key in updateFunctionHashMap){
+                        this.functions[key] = updateFunctionHashMap[key];
+                    }
+                }
+            },
+            
+            addUpdateFunction: function(mode, updateFunction){
+                this.functions[mode]= updateFunction;
+            },
+
+            /**
+             * TODO[Xavi] Té cap efecte? es sobrescrit al scriptsRef.
+             * Posa tots els botons ocults, i segons el GlobalState i la acció mostra unos botons o altres.
+             *
+             * @type {function()}
+             */
+            update: function () {
+                var currentId = dispatcher.getGlobalState().getCurrentId();
+                var f = this.functions[dispatcher.getGlobalState()
+                                                .getContentMode(currentId)];
+                f();
+            }
+        });
+    return ret;
+});
