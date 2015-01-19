97b2944fac0ccc708cbe560a71d43c9e31c96868
eduard <eduard@eduard-Minitower.(none)>
2015-01-19 12:02:36 +0100

    T031 UPDATE: AdmintabProcessor amb ContentPane

--- ioc/wiki30/processor/AdminTabProcessor.js
+++ ioc/wiki30/processor/AdminTabProcessor.js
@@ -1,57 +1,57 @@
 define([
     "dojo/_base/declare", 
     "ioc/wiki30/processor/AbstractResponseProcessor",
-    "dojo/dom",
-    "ioc/gui/ContentTabDokuwikiPage" 
-], function (declare, AbstractResponseProcessor,dom,ContentTabDokuwikiPage) {
+    "dijit/registry",
+    "dijit/layout/ContentPane"
+], function (declare, AbstractResponseProcessor,registry, ContentPane) {
     var ret = declare("ioc.wiki30.processor.AdminTabProcessor", [AbstractResponseProcessor],
+    /**
+    * @class AdminTabProcessor
+    * @extends AbstractResponseProcessor
+    */
+    {
+        type: "admintab", 
+
         /**
-         * @class AdminTabProcessor
-         * @extends AbstractResponseProcessor
-         */
-        {
-            type: "admintab", 
+        * @param {*} value
+        * @param {ioc.wiki30.Dispatcher} dispatcher
+        * @override
+        */
+        process: function (value, dispatcher) {
+            this._processAdminTab(value, dispatcher);
+        },
 
-            /**
-             * @param {*} value
-             * @param {ioc.wiki30.Dispatcher} dispatcher
-             * @override
-             */
-            process: function (value, dispatcher) {
-                this._processAdminTab(value, dispatcher);
-            },
-
-            /**
-             * @param {{containerId: string 
-                        ,tabId: string
-                        ,title: string
-                        ,content: string
-                        ,urlBase: string}} result
-             * @param {ioc.wiki30.Dispatcher} dispatcher
-             * @private
-             */
-            _processAdminTab: function (result, dispatcher) {
-              var admin_tab = dom.byId(result.containerId + "_tablist_" + result.tabId);
-              if (admin_tab !== null) {  
-                  //  Si existeix la pestanya només caldrà substituir el contingut actual pel nou
-                  dom.byId(result.tabId).innerHTML = result.content;  
-              } else {
-                 // Crear una pestanya nova a la zona de navegació si no existeix 
-                 //fill d'un objecte de tipus ContentTabDokuwikiPage 
-                 //al qual se li passi la urlBase que hagi arribat amb la resposta 
-                 //i el contingut html amb la llista de tasques.
-                 var cp1 = new ContentTabDokuwikiPage({
+        /**
+        * @param {{containerId: string 
+                    ,tabId: string
+                    ,title: string
+                    ,content: string
+                    ,urlBase: string}} result
+        * @param {ioc.wiki30.Dispatcher} dispatcher
+        * @private
+        */
+        _processAdminTab: function (result, dispatcher) {
+            var admin_tab = registry.byId(result.containerId + "_tablist_" + result.tabId);
+            if (admin_tab !== undefined) {  
+                //  Si existeix la pestanya només caldrà substituir el contingut actual pel nou
+                registry.byId(result.tabId).innerHTML = result.content;  
+                } else {
+                    // Crear una pestanya nova a la zona de navegació si no existeix 
+                    // fill d'un objecte de tipus ContentTabDokuwikiPage 
+                    //al qual se li passi la urlBase que hagi arribat amb la resposta 
+                    //i el contingut html amb la llista de tasques.
+                    //var cp1 = new ContentTabDokuwikiPage();
+                    var cp1 = new ContentPane(
+                    {
                         id: result.containerId + "_tablist_" + result.tabId,
                         title: result.title,
                         content: result.content,
                         urlBase: result.urlBase
-                });
-                var tc = dom.byId(result.containerId);
-                tc.addChild(cp1);
-              }
+                    });
+                    var tc = registry.byId(result.containerId);
+                    tc.addChild(cp1);
+                }
             }
         });
     return ret;
 });
-
-
