fa3a9d0f16531f5191ddc74317e1052a8b0f3a6f
eduard <eduard@eduard-Minitower.(none)>
2015-02-10 12:25:00 +0100

    FIX parameter missing

--- ioc/wiki30/Dispatcher.js
+++ ioc/wiki30/Dispatcher.js
@@ -27,7 +27,7 @@ define([
     "ioc/wiki30/manager/ChangesManager",
     "ioc/wiki30/UpdateViewHandler"
 ], function (declare, registry, Dialog, lang, array, GlobalState, SectokManager,
-                AlertProcessor, HtmlContentProcessor, MetaInfoProcessor,
+                AlertProcessor, HtmlContentProcessor, MediaProcessor, MetaInfoProcessor,
                 DataContentProcessor, ErrorProcessor, InfoStatusProcessor,
                 LoginProcessor, SectokProcessor, TitleProcessor,
                 RemoveAllContentTabProcessor, RemoveContentTabProcessor,
