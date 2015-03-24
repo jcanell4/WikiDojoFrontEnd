20f2e41601159f5955aba0b286e240932bb47a78
professor <mlozan54@xec.cat>
2015-03-24 20:36:34 +0100

    Canvis per fer mes agil i userfriendly l'arbre de media manager de la zona de meta

--- ioc/dokuwiki/processMetaMedia.js
+++ ioc/dokuwiki/processMetaMedia.js
@@ -17,10 +17,18 @@ define([
 
     var eventHandlers = new Array();
 
+
+
     var res = function (id, params) {
         //JSINFO.id=params.ns;
 
-
+        var $tree = jQuery('#media__tree');
+        $tree.dw_tree({toggle_selector: 'img',
+            toggle_display: function ($clicky, opening) {
+                $clicky.attr('src',
+                        DOKU_BASE + 'lib/images/' +
+                        (opening ? 'minus' : 'plus') + '.gif');
+            }});
         var domNode = dom.byId(id);
         //Es desconnecten els esdeveniments que havia connectats en aquest domNode.id
         /*dojo.forEach(connects[domNode.id], function (handle) {
@@ -32,13 +40,44 @@ define([
             this.sectok = sk;
         };
 
-        eventHandlers.push(on(domNode, 'li:click', function (e) {
+
+        eventHandlers.push(on(domNode, 'img:click', function (e) {
+            if (domClass.contains(this.parentNode, "open")) {
+                event.stop(e);
+                var unHandler;
+                while (unHandler = eventHandlers.pop()) {
+                    unHandler.remove();
+                }
+                //setCurrentElement(this);
+                var nodea = dwPageUi.getElementWhithNodeId(this.parentNode, "A");
+                //var nodea = this;
+                var elid = "";
+                //var mihref = dispatcher.getGlobalState().getCurrentElementId().href;
+                var mihref = nodea.href;
+                var andperSplit = mihref.split("&");
+                for (var i = 0; i < andperSplit.length; i++) {
+                    var igualSplit = andperSplit[i].split("=");
+                    if (igualSplit[0] === "ns") {
+                        elid = igualSplit[1];
+                    }
+                }
+                query = 'id=' + elid + '&ns=' + elid + '&do=media';
+                requestMedia.sendRequest(query);
+            }
+
+        })
+                );
+
+
+        eventHandlers.push(on(domNode, 'a:click', function (e) {
+            event.stop(e);
             var unHandler;
             while (unHandler = eventHandlers.pop()) {
                 unHandler.remove();
             }
             //setCurrentElement(this);
-            var nodea = dwPageUi.getElementWhithNodeId(this, "A");
+            //var nodea = dwPageUi.getElementWhithNodeId(this, "A");
+            var nodea = this;
             var elid = "";
             //var mihref = dispatcher.getGlobalState().getCurrentElementId().href;
             var mihref = nodea.href;
@@ -51,7 +90,7 @@ define([
             }
             query = 'id=' + elid + '&ns=' + elid + '&do=media';
             requestMedia.sendRequest(query);
-            event.stop(e);
+
         })
                 );
 
@@ -67,7 +106,7 @@ define([
                 }
             }
         })
-        );
+                );
 
         eventHandlers.push(on(domNode, '[name="filesort"]:change', function (checked) {
             var unHandler;
