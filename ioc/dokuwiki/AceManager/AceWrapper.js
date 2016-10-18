3c06308c719bdbb40af633280457dd8b5df34700
Xavier García <xaviergaro.dev@gmail.com>
2016-10-18 20:05:26 +0200

    Fix for startState

--- ioc/dokuwiki/AceManager/AceWrapper.js
+++ ioc/dokuwiki/AceManager/AceWrapper.js
@@ -120,7 +120,12 @@ define([
              */
             getLineStates: function (line, startState, tokenizer) {
                 var currentState, lastIndex, mapping, match, re, rule, state, states;
-                currentState = startState[0]; // ALERTA: El startState es un array
+
+                if (Array.isArray(startState)) {
+                    currentState = startState[0];
+                } else {
+                    currentState = startState
+                }
                 state = tokenizer.states[currentState];
                 mapping = tokenizer.matchMappings[currentState];
 
