654b1fcda8550a587947bd411861d74a127500ff
Xavier García <xaviergaro.dev@gmail.com>
2017-05-31 19:47:41 +0200

    Added drag & drop funcionality to tabs

--- ioc/gui/content/ContainerContentTool.js
+++ ioc/gui/content/ContainerContentTool.js
@@ -2,8 +2,17 @@ define([
     'dojo/_base/declare',
     'ioc/wiki30/manager/EventObservable',
     'ioc/wiki30/manager/EventObserver',
-], function (declare, EventObservable, EventObserver) {
+    'dojo/dnd/Source',
+    'dojo/dom-class',
+    'dojo/_base/array',
+    'dijit/registry',
+    'dojo/on',
+    'dojo/text!./templates/avatarTemplate.html',
+    'dojo/topic',
+    'dojo/_base/lang'
 
+
+], function (declare, EventObservable, EventObserver, Source, domClass, array, registry, on, avatarTemplate, topic, lang) {
 
 
     return declare([EventObservable, EventObserver],
@@ -70,6 +79,20 @@ define([
                     contentTool.setContainer(this);
                 }
 
+                // ALERTA[Xavi] Pot ser cal eliminar el comportament quan es tanqui la pestanya (al ondestroy?)
+                if (this.tablist) {
+                    var node = registry.byId(this.id + "_tablist_" + contentTool.id).domNode;
+                    domClass.add(node, "dojoDndItem");
+                    jQuery(node).attr('dndType', this.id);
+
+                    if (!this.dndSource) {
+                        this.initializeDragAndDrop();
+                    }
+
+                    this.dndSource.sync();
+                }
+
+
                 //this.resize(); // ALERTA[Xavi] Això es necessari? No hi ha una implementació per defecte als Containers de Dojo
 
             },
@@ -133,6 +156,59 @@ define([
                     }
                 }
                 return -1;
+            },
+
+            startup: function () {
+
+                this.inherited(arguments);
+
+                if (this.tablist) {
+                    this.initializeDragAndDrop();
+                }
+
+            },
+
+            initializeDragAndDrop: function () {
+
+                var containerNode = this.tablist.containerNode,
+                    id = this.id;
+
+                array.forEach(containerNode.children, function (node) {
+                    console.log(node);
+                    domClass.add(node, "dojoDndItem");
+                    jQuery(node).attr('dndType', id);
+
+                });
+
+
+                this.dndSource = new Source(containerNode, {
+                    withHandles: false,
+                    horizontal: true,
+                    type: this.id
+                });
+
+                on(this.dndSource, 'onStartDrag', function (source, nodes, copy) {
+                    console.log("funciona el on"); // NO FUNCIONA
+                    console.log(source, nodes, copy);
+                });
+
+
+                this.dndSource.makeAvatar = function () {
+                    console.log("override de l'avatar?"); // NO FUNCIONA. El makeAvatar pertany a dnd/Manager
+                };
+
+
+                function highlightTargets(show, source, nodes) {
+                    for (var i = 0; i < source.node.children.length; i++) {
+                        if (source.node.childNodes[i] !== nodes[0]) {
+                            domClass.toggle(source.node.childNodes[i], "highlight", show);
+                        }
+                    }
+                }
+
+                topic.subscribe("/dnd/start", lang.partial(highlightTargets, true));
+
+                topic.subscribe("/dnd/cancel, /dnd/drop", lang.partial(highlightTargets, false));
             }
         });
 });
