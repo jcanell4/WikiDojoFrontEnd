3f00ddb61aec22fa8a7873e913aa3dcd08abadec
Xavier García <xaviergaro.dev@gmail.com>
2017-06-08 14:50:48 +0200

    Removed unused code

--- ioc/gui/content/ContainerContentTool.js
+++ ioc/gui/content/ContainerContentTool.js
@@ -70,18 +70,6 @@ define([
                     contentTool.setContainer(this);
                 }
 
-                // ALERTA[Xavi] Això no s'utilitza, és el codi de prova del drag & drop
-                // if (this.tablist) {
-                //     var node = registry.byId(this.id + "_tablist_" + contentTool.id).domNode;
-                //     domClass.add(node, "dojoDndItem");
-                //     jQuery(node).attr('dndType', this.id);
-                //
-                //     if (!this.dndSource) {
-                //         this.initializeDragAndDrop();
-                //     }
-                //
-                //     this.dndSource.sync();
-                // }
 
 
 
@@ -165,48 +153,6 @@ define([
 
             },
 
-            //ALERTA[Xavi] Això no s'utilitza, és el codi de prova del drag & drop
-            // initializeDragAndDrop: function () {
-            //
-            //     var containerNode = this.tablist.containerNode,
-            //         id = this.id;
-            //
-            //     array.forEach(containerNode.children, function (node) {
-            //         domClass.add(node, "dojoDndItem");
-            //         jQuery(node).attr('dndType', id);
-            //
-            //     });
-            //
-            //
-            //     this.dndSource = new Source(containerNode, {
-            //         withHandles: false,
-            //         horizontal: true,
-            //         type: this.id
-            //     });
-            //
-            //     on(this.dndSource, 'onStartDrag', function (source, nodes, copy) {
-            //         console.log("funciona el on"); // NO FUNCIONA
-            //         console.log(source, nodes, copy);
-            //     });
-            //
-            //
-            //     this.dndSource.makeAvatar = function () {
-            //         console.log("override de l'avatar?"); // NO FUNCIONA. El makeAvatar pertany a dnd/Manager
-            //     };
-            //
-            //
-            //     function highlightTargets(show, source, nodes) {
-            //         for (var i = 0; i < source.node.children.length; i++) {
-            //             if (source.node.childNodes[i] !== nodes[0]) {
-            //                 domClass.toggle(source.node.childNodes[i], "highlight", show);
-            //             }
-            //         }
-            //     }
-            //
-            //     topic.subscribe("/dnd/start", lang.partial(highlightTargets, true));
-            //
-            //     topic.subscribe("/dnd/cancel, /dnd/drop", lang.partial(highlightTargets, false));
-            // },
 
             addDisplacementArrowsToTab: function(contentTool) {
                 var tabController = this.tablist;
