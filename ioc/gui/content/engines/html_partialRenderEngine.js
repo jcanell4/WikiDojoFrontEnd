d059b6547721a572f40746bb3fe1bea085037155
professor <professor@debian8>
2017-09-06 11:10:24 +0200

    Correcció d'un error que impedia obtenir una pàgina amb errors sintàctics de la WIKI

--- ioc/gui/content/engines/html_partialRenderEngine.js
+++ ioc/gui/content/engines/html_partialRenderEngine.js
@@ -18,7 +18,10 @@ define(function () {
 
         $doc = jQuery('<div>' + data.html + '</div><div class="end-of-document></div>"');
 
-        var editing_chunks = [];
+        try {
+
+
+            var editing_chunks = [];
 
         for (i = 0; i < data.chunks.length; i++) {
             if (data.chunks[i].text) {
@@ -123,6 +126,13 @@ define(function () {
             $divInsideForm.append('<input type="hidden" value="' + id + '" name="section_id"/>');
             $divInsideForm.append('<input name="editing_chunks" value="' + editing_chunks + '" type="hidden">');
         });
+
+        } catch (e){
+            var message = "S'ha produït un error. S'ha desactivat la edició parcial";
+            alert(message);
+            console.error(message);
+            $doc = jQuery('<div>' + data.html + '</div><div class="end-of-document></div>"');
+        }
 
         return $doc;
     }
