6148fb446a0d6bbc1d1e142b4b2b6e99ce9d1c58
rclaver <rclaver@xtec.cat>
2018-03-15 12:16:10 +0100

    correcció del callback en els botons d'elecció de docu-draft

--- ioc/wiki30/processor/ProjectContentProcessor.js
+++ ioc/wiki30/processor/ProjectContentProcessor.js
@@ -45,11 +45,13 @@ define([
          */
         _showDiffDialog: function (value, draft, args) {
 
-            var context = this;
+            var contentTool = this.draftManager.drafts[value.ns].contentTool;
             var data = {
                 document: this._getDocument(value),
                 draft: this._getDraft(draft)
             };
+            var dataDocum = this._convertUnixDate(data.document.date);
+            var dataDraft = this._convertUnixDate(data.draft.date);
 
             var dialogParams = {
                 id: "project_diff",
@@ -64,7 +66,7 @@ define([
                         description: "Editar el formulari original del projecte",
                         buttonType: 'default',
                         callback: function(){
-                            context.inherited("process", args);
+                            contentTool.updateDocument(args[0]);
                         }
                     },
                     {
@@ -73,15 +75,15 @@ define([
                         buttonType: 'default',
                         callback: function(){
                             args[0].content.formValues = JSON.parse(draft.content);
-                            context.inherited("process", args);
+                            contentTool.updateDocument(args[0]);
                         }
                     }
                 ],
                 diff: {
                     formDocum: data.document.content,
                     formDraft: data.draft.content,
-                    labelDocum: "Document (" + data.document.date + ")",
-                    labelDraft: "Esborrany (" + data.draft.date + ")"
+                    labelDocum: "Formulari original (" + dataDocum + ")",
+                    labelDraft: "Esborrany (" + dataDraft + ")"
                 }
             };
 
@@ -95,6 +97,19 @@ define([
 
         _getDraft: function (draft) {
             return {content: draft.content, date: draft.date};
+        },
+        
+        _convertUnixDate: function (fecha) {
+            var p = 13 - fecha.toString().length; //He detectado fechas con menos dígitos de lo normal
+            if (p > 0) {
+                var mul = 1;
+                for (var i=0; i<p; i++) {
+                    mul *= 10;
+                }
+                fecha *= mul;
+            }
+            var d = new Date(fecha);
+            return d.getDate() + "-" + (d.getMonth()+1) + "-" + d.getFullYear();
         },
         
         createContentTool: function (content, dispatcher) {
