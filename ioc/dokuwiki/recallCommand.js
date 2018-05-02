9776ff9abf07619e6de73112dfdf2bcf10a33f1d
rclaver <rclaver@xtec.cat>
2018-05-02 08:33:40 +0200

    limpieza

--- ioc/dokuwiki/recallCommand.js
+++ ioc/dokuwiki/recallCommand.js
@@ -5,22 +5,16 @@
 define([
     "ioc/wiki30/Request"
 ], function(Request){
+    
     var requestUpdate = new Request();
-//    requestUpdate.updateSectok=function(sk){
-//            this.sectok=sk;
-//    };
-//    requestUpdate.sectok = requestUpdate.dispatcher.getSectok();
-//    requestUpdate.dispatcher.toUpdateSectok.push(requestUpdate);
-
 
     var res = function(params){
-
         requestUpdate.urlBase=params.urlBase;
-        if(params.method && params.method.toLowerCase()=="post"){
+        if(params.method && params.method.toLowerCase()==="post"){
             requestUpdate.method="post";
             requestUpdate.getPostData = function(){
-                return params.data
-            }
+                return params.data;
+            };
         }else{
             requestUpdate.method="get";
         }
