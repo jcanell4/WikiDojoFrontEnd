d5b0571f0d476b14fc97f939b97c4ce27de0690e
rclaver <rclaver@xtec.cat>
2018-05-31 15:06:04 +0200

    reestructuración completa para _addButton

--- ioc/gui/IocContentPane.js
+++ ioc/gui/IocContentPane.js
@@ -1,46 +1,56 @@
 define([
-    "dojo/_base/declare", // declare
+    "dojo/_base/declare",
     "dojo/dom-construct",
-    "dojo/dom-style",
-    "dijit/layout/ContentPane",
-    "ioc/gui/IocResizableComponent"
-
-], function(declare, domConstruct, domStyle, ContentPane, IocComponent) {
-    return declare("ioc.gui.IocContentPane", [ContentPane, IocComponent],
+    "dijit/layout/ContentPane"
+], function(declare, domConstruct, ContentPane) {
+    return declare("ioc.gui.IocContentPane", [ContentPane],
     {
-        buttons:null,
-        _toolBars:null,
-        startup: function(){
-            this.inherited(arguments);
+        /* buttons es un array de botones. cada elemento del array contiene:
+            'amdClass': nom de la classe, 
+            'position': contiene 1 valor a elegir entre: topLeft, topRight, bottomLeft, bottomRight, 
+            'buttonParams': objeto con los parámetros del tipo de botón */
+        buttons: null,
+        _toolBars: null,
+
+        postStartup: function(){
             for(var i=0; this.buttons && i<this.buttons.length; i++){
-                this._addButton(this.buttons[i].ambClass, this.buttons[i].position, this.buttons[i].buttonParams);
+                this._addButton(this.buttons[i].amdClass, this.buttons[i].position, this.buttons[i].buttonParams);
             }
         },
+        
         _addButton: function(amdButton, buttonPos, buttonParams){
-            if(buttonParams.class){
+            if (this._toolBars === null){
+                var htmlDIV = "<div style='position:absolute; top:2px; left:2px; display:block; z-index:1000'></div>";
+                //La posició real s'estableix amb el métode onResize de Contentool.js
+                var topLeftNode = domConstruct.toDom(htmlDIV);
+                var topRightNode = domConstruct.toDom(htmlDIV);
+                var bottomLeftNode = domConstruct.toDom(htmlDIV);
+                var bottomRightNode = domConstruct.toDom(htmlDIV);
+
+                this._toolBars = {topLeft:topLeftNode, topRight:topRightNode, bottomLeft:bottomLeftNode, bottomRight:bottomRightNode};
+                
+                domConstruct.place(topLeftNode, this.domNode); 
+                domConstruct.place(topRightNode, this.domNode); 
+                domConstruct.place(bottomLeftNode, this.domNode); 
+                domConstruct.place(bottomRightNode, this.domNode); 
+            }
+            
+            var self = this;
+            if (buttonParams.class){
                 buttonParams.class += " contentPaneButton";
             }else{
                 buttonParams.class = "contentPaneButton";
             }
-            var self = this;
-            if(this._toolBars==null){
-                var domNodeComputedStyle = domStyle.getComputedStyle(this.domNode);
-                var topLeftNode = domConstruct.toDom("<div style='position: absolute; top: 2px; left: 2px; display: block; z-index: 1000'></div>");
-                var topRightNode = domConstruct.toDom("<div style='position: absolute; top: 2px; left: "+domNodeComputedStyle.width+"px; display: block; z-index: 1000'></div>");
-                var bottomLeftNode = domConstruct.toDom("<div style='position: absolute; top: "+domNodeComputedStyle.height+"px; left: 2px; display: block; z-index: 1000'></div>");
-                var bottomRightNode = domConstruct.toDom("<div style='position: absolute; top: "+domNodeComputedStyle.height+"px; left: "+domNodeComputedStyle.width+"px; display: block; z-index: 1000'></div>");
-                domConstruct.place(topLeftNode, this.domNode);
-                this._toolBars = {topLeft:topLeftNode, topRight:topRightNode, bottomLeft:bottomLeftNode, bottomRight: bottomRightNode};
-            }
+            
             require([amdButton], function(Button){
                 var button = new Button(buttonParams);
-                if(buttonPos=='topLeft'){
-                    domConstruct.place(button.domNode, self._toolBars.topLef);
-                }else if(buttonPos=='topRight'){
+                if (buttonPos==='topLeft'){
+                    domConstruct.place(button.domNode, self._toolBars.topLeft);
+                }else if(buttonPos==='topRight'){
                     domConstruct.place(button.domNode, self._toolBars.topRight);
-                }else if(buttonPos=='bottomLeft'){
+                }else if(buttonPos==='bottomLeft'){
                     domConstruct.place(button.domNode, self._toolBars.bottomLeft);
-                }else if(buttonPos=='bottomRight'){
+                }else if(buttonPos==='bottomRight'){
                     domConstruct.place(button.domNode, self._toolBars.bottomRight);
                 }
             });
