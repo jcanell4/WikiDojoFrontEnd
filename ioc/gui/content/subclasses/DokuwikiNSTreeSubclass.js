f44c42895787b56bb198b8c76efb093f44d233e6
rclaver <rclaver@xtec.cat>
2018-05-31 15:05:11 +0200

    ejecutamos un extra de IocContentPane

--- ioc/gui/content/subclasses/DokuwikiNSTreeSubclass.js
+++ ioc/gui/content/subclasses/DokuwikiNSTreeSubclass.js
@@ -1,9 +1,8 @@
 define([
-    "dojo/_base/declare",
-], function (declare, ContentTabDokuwikiNsTree) {
+    "dojo/_base/declare"
+], function (declare) {
 
     return declare([], {
-
 
         constructor: function (args) {
             this.params = args;
@@ -12,30 +11,28 @@ define([
         startup: function () {
             this.inherited(arguments);
             this._createTree(this, this.params);
-
+            
+            // ejecutamos un extra de IocContentPane una vez el ContentPane se ha llenado con el NsTree
+            if (this.postStartup) {
+                this.postStartup();
+            }
         },
 
         _createTree: function (context, params) {
             this.params = params;
 
             require(["ioc/gui/ContentTabDokuwikiNsTree"], function (ContentTabDokuwikiNsTree) {
-
                 params.id = params.id + "_tree";
-
-
                 context.tree = new ContentTabDokuwikiNsTree(params).placeAt(context);
                 context.tree.startup();
-
             });
         },
 
         updateDocument: function (content) {
-            //console.log("DokuwikiNSTresSubclass#updateDocument", content);
             // ALERTA[Xavi] El mètode refresh() de l'arbre no funciona quan es crida aquí, es perden les referencies al rootNode
             this.tree.destroyRecursive();
             this._createTree(this, content);
         }
-
 
     });
 
