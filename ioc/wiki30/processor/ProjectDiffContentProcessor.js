8d85bfbbbcb9cede393e6d3b67610db607c77a27
rclaver <rclaver@xtec.cat>
2018-06-13 14:47:32 +0200

    _convertUnixDate

--- ioc/wiki30/processor/ProjectDiffContentProcessor.js
+++ ioc/wiki30/processor/ProjectDiffContentProcessor.js
@@ -82,15 +82,19 @@ define([
         },
 
         _convertUnixDate: function (fecha, hora) {
-            var p = 13 - fecha.toString().length; //He detectado fechas con menos dígitos de lo normal
-            if (p > 0) {
-                var mul = 1;
-                for (var i=0; i<p; i++) {
-                    mul *= 10;
+            if (fecha) {
+                var p = 13 - fecha.toString().length; //He detectado fechas con menos dígitos de lo normal
+                if (p > 0) {
+                    var mul = 1;
+                    for (var i=0; i<p; i++) {
+                        mul *= 10;
+                    }
+                    fecha *= mul;
                 }
-                fecha *= mul;
+                var d = new Date(fecha);
+            }else {
+                var d = new Date();
             }
-            var d = new Date(fecha);
             var ret = d.getDate() + "." + (d.getMonth()+1) + "." + d.getFullYear();
             if (hora) 
                 ret += " - " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
