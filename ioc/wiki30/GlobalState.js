2819dd77b78b9ec5e126fb32358bfaa0701e4650
Xavier García <xaviergaro.dev@gmail.com>
2019-09-12 11:59:08 +0200

    Fixed null page added on reload

--- ioc/wiki30/GlobalState.js
+++ ioc/wiki30/GlobalState.js
@@ -200,7 +200,11 @@ define([
         getContent: function (id) {
             var ret = undefined;
 
-            if (this.pages[id]) {
+
+            // Aquest cas es dona quan no hi ha cap document
+            if (id === null) {
+                ret = {};
+            } else if (this.pages[id]) {
                 ret = this.pages[id];
             } else {
                 ret = this.pages[id] = {};
@@ -481,7 +485,7 @@ define([
         },
 
         updateSessionStorage: function () {
-            // console.log("GlobalState#updateSessionStorage", this);
+             console.log("GlobalState#updateSessionStorage", this);
             // Update del sessionStorage, això és el que es fa ara en recarregar la pàgina
             storageManager.setObject('globalState', this);
         },
