42fff06b70493e7386dbfe99202e2e6dc508e874
rclaver <rclaver@xtec.cat>
2020-03-24 20:01:41 +0100

    delay al procés de recall

--- ioc/dokuwiki/recallCommand.js
+++ ioc/dokuwiki/recallCommand.js
@@ -1,5 +1,5 @@
 /**
-* Funció que torna a creidar la comanda que arriba per paràmetres
+* Funció que torna a cridar la comanda que arriba per paràmetres
 * @author Josep Cañellas & Eduard Latorre
 */
 define([
@@ -7,21 +7,29 @@ define([
 ], function(Request){
     
     var requestUpdate = new Request();
+    
+    function sleep(delay) {
+        if (delay===undefined || delay<0) delay = 0;
+        return new Promise((resolve) => setTimeout(resolve, delay));
+    }
 
     var res = function(params){
-        requestUpdate.urlBase=params.urlBase;
-        if(params.method && params.method.toLowerCase()==="post"){
-            requestUpdate.method="post";
+        requestUpdate.urlBase = params.urlBase;
+        if (params.method && params.method.toLowerCase()==="post"){
+            requestUpdate.method = "post";
             requestUpdate.getPostData = function(){
                 return params.data;
             };
         }else{
             requestUpdate.method="get";
         }
-        if(params.query){
+        if (params.query){
             requestUpdate.query = params.query;
         }
-        requestUpdate.sendRequest();
+        sleep(params.delay).then(function() {
+            requestUpdate.sendRequest();
+        });
     };
     return res;
+    
 });
